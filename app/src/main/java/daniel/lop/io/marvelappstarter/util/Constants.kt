be3edf822bd47ddfefddb2824c5422ccad9a3385
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2021-11-13 10:38:49 -0300

    implementa pesquisa de personagens

--- app/src/main/java/daniel/lop/io/marvelappstarter/util/Constants.kt
+++ app/src/main/java/daniel/lop/io/marvelappstarter/util/Constants.kt
@@ -7,6 +7,9 @@ object Constants {
     const val PUBLIC_KEY = "a42344fefad2b81dbe026c7becbcffa5"
     const val PRIVATE_KEY = "7a0eb3edfafffe9fe747e60ae0853908d1a19825"
 
+    const val LAST_SEARCH_QUERY = "last_search_query"
+    const val DEFAULT_QUERY = ""
+
     const val APIKEY = "apikey"
     const val HASH = "hash"
     const val TS = "ts"
