9655044ddb22853259eb7b84be50717ae046551b
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2021-11-13 00:46:09 -0300

    implementa a listagem em ListCharacterFragment

--- app/src/main/java/daniel/lop/io/marvelappstarter/util/Constants.kt
+++ app/src/main/java/daniel/lop/io/marvelappstarter/util/Constants.kt
@@ -2,7 +2,7 @@ package daniel.lop.io.marvelappstarter.util
 
 object Constants {
 
-    const val BASE_URL = "https://gateway.marvel.com/V1/public/"
+    const val BASE_URL = "https://gateway.marvel.com/v1/public/"
 
     const val PUBLIC_KEY = "a42344fefad2b81dbe026c7becbcffa5"
     const val PRIVATE_KEY = "7a0eb3edfafffe9fe747e60ae0853908d1a19825"
